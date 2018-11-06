73d27dad1f4365915ec704e074d5951d82956cdf
Ruhul <ruhul20@gmail.com>
2018-11-06 15:12:30 +0600

    Add files via upload

---
+++ even odd check.asm
@@ -0,0 +1,22 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+mov ax,7
+test ax,1
+JZ even
+JNZ odd
+
+even :
+    mov bx,1
+
+odd :
+    mov bx,0
+
+ret
+
+
+
+
