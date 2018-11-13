391f5f4aba148e43bf4f0e6dad6b2d00baac8019
Ruhul <ruhul20@gmail.com>
2018-11-13 14:38:06 +0600

    Update basic input output.asm

--- basic input output.asm
+++ basic input output.asm
@@ -7,11 +7,18 @@ org 100h
 ; single character input
 mov ah,1
 INT 21H
+mov bl,al
+;cr=0dh,lf=0sh for new line
+mov ah,2
+mov dl,0AH
+int 21H
+mov dl,0DH
+int 21H 
 ;will find it in al register
 ;single character output
 ;dl is to display
 mov ah,2
-mov dl,al
+mov dl,bl
 ;mov dl,'a' to display a and al=dl
 INT 21H
 ret
