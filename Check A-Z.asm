75192c2d6237e7748e604c2dc3ef4e82fa5632f3
Ruhul <ruhul20@gmail.com>
2018-11-13 14:58:11 +0600

    Add files via upload

---
+++ Check A-Z.asm
@@ -0,0 +1,59 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+mov ah,1
+INT 21H
+mov bl,al
+mov ah,2
+mov dl,0AH
+int 21H
+mov dl,0DH
+int 21H 
+cmp bl,'A'
+jae greater
+    mov dl,'f'
+    int 21H
+    mov dl,'a'
+    int 21H
+    mov dl,'l'
+    int 21H
+    mov dl,'s'
+    int 21H        
+    mov dl,'e'
+    int 21H
+    hlt  
+
+greater:
+    cmp bl,'Z'
+    jbe true
+    mov dl,'f'
+    int 21H
+    mov dl,'a'
+    int 21H
+    mov dl,'l'
+    int 21H
+    mov dl,'s'
+    int 21H        
+    mov dl,'e'
+    int 21H    
+    hlt
+true:
+    mov dl,'t'
+    int 21H
+    mov dl,'r'
+    int 21H
+    mov dl,'u'
+    int 21H
+    mov dl,'e'
+    int 21H
+    hlt
+        
+
+ret
+
+
+
+
